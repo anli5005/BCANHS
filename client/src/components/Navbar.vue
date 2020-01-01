2a95a0bb57c709916d7c8b5f124d3459bb17204e
Aidan Glickman <aidan@aidanglickman.com>
2019-12-31 23:14:56 -0500

    build navbar

--- client/src/components/Navbar.vue
+++ client/src/components/Navbar.vue
@@ -3,45 +3,33 @@
     <b-navbar toggleable="lg" type="dark" variant="dark">
       <b-container>
         <b-navbar-brand href="#">
-          <b-img src="@/assets/images/logo.png"></b-img>
+          <img
+            src="~@/assets/images/nhs_logo_light.png"
+            height="30"
+            width="30"
+          />
+          BCA NHS
         </b-navbar-brand>
 
         <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
 
         <b-collapse id="nav-collapse" is-nav>
-          <b-navbar-nav>
-            <b-nav-item href="#">Link</b-nav-item>
-            <b-nav-item href="#" disabled>Disabled</b-nav-item>
-          </b-navbar-nav>
-
           <!-- Right aligned nav items -->
           <b-navbar-nav class="ml-auto">
-            <b-nav-form>
-              <b-form-input
-                size="sm"
-                class="mr-sm-2"
-                placeholder="Search"
-              ></b-form-input>
-              <b-button size="sm" class="my-2 my-sm-0" type="submit"
-                >Search</b-button
+            <b-nav-item-dropdown text="Info">
+              <b-dropdown-item to="/info/bylaws">Bylaws</b-dropdown-item>
+              <b-dropdown-item to="/info/requirements"
+                >Membership Requirements</b-dropdown-item
               >
-            </b-nav-form>
-
-            <b-nav-item-dropdown text="Lang" right>
-              <b-dropdown-item href="#">EN</b-dropdown-item>
-              <b-dropdown-item href="#">ES</b-dropdown-item>
-              <b-dropdown-item href="#">RU</b-dropdown-item>
-              <b-dropdown-item href="#">FA</b-dropdown-item>
             </b-nav-item-dropdown>
-
-            <b-nav-item-dropdown right>
-              <!-- Using 'button-content' slot -->
-              <template v-slot:button-content>
-                <em>User</em>
-              </template>
-              <b-dropdown-item href="#">Profile</b-dropdown-item>
-              <b-dropdown-item href="#">Sign Out</b-dropdown-item>
+            <b-nav-item-dropdown text="Board">
+              <b-dropdown-item to="/board">Board Members</b-dropdown-item>
+              <b-dropdown-item to="/board/videos"
+                >Campaign Videos</b-dropdown-item
+              >
             </b-nav-item-dropdown>
+            <b-nav-item to="/tutoring">Tutoring</b-nav-item>
+            <b-nav-item href="/login">Login</b-nav-item>
           </b-navbar-nav>
         </b-collapse>
       </b-container>
