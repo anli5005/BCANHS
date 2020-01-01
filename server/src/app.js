db058e1e25af98baf832a4f03793a5e72d2ff2e2
Aidan Glickman <aidan@aidanglickman.com>
2020-01-01 00:03:27 -0500

    about to start db translator to pull all sessions

--- server/src/app.js
+++ server/src/app.js
@@ -2,6 +2,21 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
 const morgan = require("morgan");
+const mongoose = require("mongoose");
+const passport = require("passport");
+const OutlookStrategy = require("passport-outlook").Strategy;
+
+require("dotenv").config();
+
+mongoose.connect(process.env.DB_URL);
+
+var tutorSessSchema = new mongoose.Schema({
+  tutorID: String,
+  subjects: [String],
+  availability: [[Number]],
+});
+
+var tutorSess = mongoose.model("tutorSess", tutorSessSchema);
 
 const app = express();
 
