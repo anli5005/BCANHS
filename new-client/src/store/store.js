8f768eefc4ad9b3bb45922f884ea56179836375b
davidionita <31045833+davidionita@users.noreply.github.com>
2020-05-09 21:23:27 -0400

    Added vuex-persistedstate so user is still logged in after refreshing page
    Decided to use session storage instead of cookies to avoid storage of sensitive data in cookies when broswer closes.  Plus, I did not want to send state data to the server through cookies.
    
    Also, I made sure state.token is cleared on user Logout to avoid malicious use.

--- new-client/src/store/store.js
+++ new-client/src/store/store.js
@@ -1,10 +1,14 @@
 import Vue from "vue";
 import Vuex from "vuex";
+import createPersistedState from 'vuex-persistedstate'
 
 Vue.use(Vuex);
 
 export default new Vuex.Store({
   strict: true,
+  plugins: [createPersistedState({
+    storage: window.sessionStorage,
+  })],
   state: {
     token: null,
     user: null,
@@ -17,6 +21,8 @@ export default new Vuex.Store({
       state.isLoggedIn = true;
     },
     logOut(state) {
+      sessionStorage.clear();
+      state.token = null;
       state.user = null;
       state.isLoggedIn = false;
     }
